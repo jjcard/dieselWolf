5ff9941ef16ca236755fe9081ce1221cfed5a3ae
John Hill <brashcaran@gmail.com>
2015-03-31 15:25:07 -0700

    removed the color printing function, not using it

--- csc180Winner/gameLoop.cpp
+++ csc180Winner/gameLoop.cpp
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
 #include <vector>
@@ -72,21 +71,6 @@ bool isMovePossibleMin(int re[][4], int count, int from_row, int from_col, int t
 void gameOver(bool maxPlayerOne);
 bool isPlayerGoingFirst();
 char getPrintValue(int value);
-#define RESET 0
-#define BRIGHT 1
-#define DIM 2
-#define UNDERLINE 3
-#define BLINK 4
-#define REVERSE 7
-#define HIDDEN 8
-#define BLACK 0
-#define RED 1
-#define GREEN 2
-#define YELLOW 3
-#define BLUE 4
-#define MAGENTA 5
-#define CYAN 6
-#define WHITE 7
 
 
 
@@ -807,12 +791,6 @@ void printboard(){
 	cout << "--------------------------" << endl;
 	cout << "   A  B  C  D  E  F  G  H" << endl;
 }
-void textcolor(int attr, int fg, int bg){
-	char command[13];
-	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
-	printf("%s", command);
-}
-
 
 
 
